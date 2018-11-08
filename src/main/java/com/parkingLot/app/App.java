97841e64c0dfc6f6ef544fd665bbe2ba67aa5b06
demonnick <georgefort.nic@gmail.com>
2018-11-08 14:02:14 +0200

    redo

---
+++ src/main/java/com/parkingLot/app/App.java
@@ -0,0 +1,19 @@
+package com.parkingLot.app;
+
+// import the rest service you created!
+
+import com.parkingLot.rest.RestService;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+public class App extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+    public App() {
+        // Register our hello service
+        singletons.add(new RestService());
+    }
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
