990a7dfe9b8c2faeabff8ee45333ecef6295c4b8
demonnick <georgefort.nic@gmail.com>
2018-11-09 14:48:27 +0200

    11/9/18
    Working rest, DAO, DB.

---
+++ src/main/java/com/parkingLot/data/service/HsqlDatabase.java
@@ -0,0 +1,42 @@
+package com.parkingLot.data.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HsqlDatabase {
+	public Connection conn;
+
+	// Connecting to database =>
+	// Executing Query
+	public HsqlDatabase() {
+		try {
+			loadJdbcDriverForHsqlDb();
+			setupConnection();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void setupConnection() throws SQLException {
+		conn = DriverManager.getConnection("jdbc:hsqldb:file:C:/Users/fornic01/Desktop/parkingLot/src/main/webapp/WEB-INF\\PARKING/", "sa", "");
+	}
+
+	private void loadJdbcDriverForHsqlDb() throws ClassNotFoundException {
+		Class.forName("org.hsqldb.jdbcDriver");
+	}
+
+	private void shutdownHsqlDatabase() throws SQLException {
+		Statement st = conn.createStatement();
+		st.execute("SHUTDOWN");
+	}
+
+	public void closeConnection() throws SQLException {
+		shutdownHsqlDatabase();
+		conn.close(); // if there are no other open connection
+	}
+}
--- src/main/java/com/parkingLot/rest/RestService.java
+++ src/main/java/com/parkingLot/rest/RestService.java
@@ -4,13 +4,29 @@ package com.parkingLot.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import com.parkingLot.data.service.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
 
 @Path("/")
 public class RestService {
+
+TodoDataService cry = new TodoDataService();
+
     @GET // This annotation indicates GET request
     @Path("/hello")
-    public Response hello() {
-        return Response.status(200).entity("hello").build();
+    public String hello() {
+        try {
+            return cry.retrieveAllTodos().get(0).toString();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
